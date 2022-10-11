7e075879b5c1ce03822ce3114b37979ebc5dbadc
wasihun-code <wasihunageru7947@gmail.com>
2022-10-11 12:14:35 +0530

    Event listner added and functions implemented for game

---
+++ script.js
@@ -0,0 +1,121 @@
+// declare some global variables
+let player = "";
+let playerCount = 0;
+let computerCount = 0;
+let drawCount = 0;
+
+// Function: Generates proper random integer
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+// Function: return random string from an array
+//            using random generator function
+function getComputerChoice() {
+    const choice = ["rock", "paper", "scissors"];
+
+    let index = getRndInteger(0, 3);
+    return choice[index];
+}
+
+// Function: Play one round of rock paper scissor
+//         : between computer(randomly generated) and player
+function playRound(player, computer) {
+
+    if (player === computer)
+        return 3;
+
+    else if (player === "rock") {
+        if (computer === "paper")
+            return 2;
+        else if (computer === "scissors")
+            return 1;
+    }
+
+    else if (player === "paper") {
+        if (computer === "scissors")
+            return 2;
+        else
+            return 1;
+    }
+
+    else {
+        if (computer === "paper")
+            return 1;
+        else
+            return 2;
+    }
+}
+
+// Create 3 buttons
+let btnRock = document.createElement('button');
+let btnPaper = document.createElement('button');
+let btnScissors = document.createElement('button');
+
+// Add inner text
+btnRock.innerText = "Rock";
+btnPaper.innerText = "Paper";
+btnScissors.innerText = "Scissors";
+
+// Add event Listner for each button
+btnRock.addEventListener('click', function (e) {
+    player = e.target.innerText;
+    playThreeRounds();
+});
+btnPaper.addEventListener('click', function (e) {
+    player = e.target.innerText;
+    playThreeRounds();
+});
+btnScissors.addEventListener('click', function (e) {
+    player = e.target.innerText;
+    playThreeRounds();
+});
+
+function playThreeRounds() {
+
+    // Game resets after each one won
+    if (playerCount === 3 || computerCount === 3) {
+        displayResult();
+        playerCount = drawCount = computerCount = 0;
+        return;
+    }
+    h1.style.display = "none";
+    player = player.toLowerCase();
+    computer = getComputerChoice();
+
+    // Play One Round
+    let result = playRound(player, computer);
+
+    (result === 1) ? playerCount++ :
+    (result === 2) ? computerCount++ :
+                     drawCount++;
+}
+
+
+// Create div and append all buttons to it
+let h1 = document.createElement('h1');
+h1.innerText = "Player Won";
+h1.style.display = "none";
+
+let div = document.createElement('div');
+div.append(btnRock, btnPaper, btnScissors, h1);
+
+function displayResult() {
+    
+    if (computerCount === 3) {
+        h1.innerText = "Computer Won";
+    }
+    h1.style.display = "inline";
+}
+
+
+
+// Display the buttons
+document.body.append(div)
+
+// Add classes to each button, div for styling
+
+
+// Styling:
+div.style.display = "flex";
+div.style.flexDirection = "column";
